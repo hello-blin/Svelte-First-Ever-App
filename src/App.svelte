cc2338c081abfbb4febf7a3a98682aafdbaacbb5
Blin Kukaj <kblin164@gmail.com>
2022-06-02 21:22:07 +0200

    First ever stylized Svelte app

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import "./styles.css";
+	export let name, age;
+
+	$: uppercaseName = name.toUpperCase();
+
+	function changeName() {
+		name = prompt();
+	}
+</script>
+
+<div class="hayo">
+	<div class="title-container">
+		<h1>
+			Hello {uppercaseName} and {age + 1}!
+		</h1>
+	</div>
+	<div class="button-container">
+		<button on:click={changeName}>Change Name</button>
+	</div>
+</div>
