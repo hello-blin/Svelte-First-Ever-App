a7f75fa430828895ac1afaabd2e764936b430e0b
Blin Kukaj <kblin164@gmail.com>
2022-06-02 21:30:42 +0200

    Final Commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script>
 	import "./styles.css";
 	export let name, age;
-
 	$: uppercaseName = name.toUpperCase();
 
 	function changeName() {
@@ -12,7 +11,7 @@
 <div class="hayo">
 	<div class="title-container">
 		<h1>
-			Hello {uppercaseName} and {age + 1}!
+			Hello my name's {uppercaseName} and I'm {age + 1} years old!
 		</h1>
 	</div>
 	<div class="button-container">
